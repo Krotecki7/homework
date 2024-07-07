d2d48c872bb7e863ddf90644e9516ff81d53be34
Krotov <krot.playstation@gmail.com>
2024-07-07 19:33:41 +0300

    Создана функция для получения корректной даты

--- src/widget.py
+++ src/widget.py
@@ -1,4 +1,7 @@
+from datetime import datetime
+
 from src.masks import get_mask_card_number, get_mask_account
+
 
 def mask_account_card(user_card: str) -> str | None:
     """Функция, обрабатывающая информацию о картах"""
@@ -11,3 +14,12 @@ def mask_account_card(user_card: str) -> str | None:
 
 
 print(mask_account_card("Счет 64686473678894779589"))
+
+
+def get_date(date_string: str) -> str:
+    """Функция для возврата даты в привычном значении"""
+    date = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%S.%f')
+    return date.strftime('%d.%m.%Y')
+
+
+print(get_date('2024-03-11T02:26:18.671407'))
