0184d2f7cc8c7e96cefb52d7391151d16a27b7d0
Krotov <krot.playstation@gmail.com>
2024-07-17 08:04:26 +0300

    Тест модуля masks.py

---
+++ tests/test_masks.py
@@ -0,0 +1,27 @@
+import pytest
+
+from src.masks import get_mask_card_number, get_mask_account
+
+
+@pytest.fixture
+def mask_card():
+    return '7000792289606361'
+
+
+def test_get_mask_card_number(mask_card):
+    assert get_mask_card_number(mask_card) == '7000 79** **** 6361', "Неправильно набран номер"
+    assert get_mask_card_number("") is None, "Введите номер карты"
+    assert get_mask_card_number('1234556778991234324123134') is None, "Слишком длинный номер"
+    assert get_mask_card_number('123455') is None, "Короткий номер"
+
+
+@pytest.fixture
+def mask_account():
+    return '73654108430135874305'
+
+
+def test_get_mask_account(mask_account):
+    assert get_mask_account(mask_account) == '**4305', "Некорректный счет"
+    assert get_mask_account('') is None, "Введите номер карты"
+    assert get_mask_account('1234566787899121421553') is None, "Слишком длинный номер"
+    assert get_mask_account('123445') is None, "Короткий номер"
