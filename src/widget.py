9ef080f28d77c876eabe8cf665ef8531ecb01475
Krotov <krot.playstation@gmail.com>
2024-07-17 17:32:32 +0300

    Изменены некоторые функции, исходя из результатов тестов

--- src/widget.py
+++ src/widget.py
@@ -7,16 +7,13 @@ def mask_account_card(user_card: str) -> str | None:
     """Функция получает номер счета или название и номер карты, и возвращает их в замаскированном виде."""
     if "Счет" in user_card:
         account = user_card[-20:]
-        return f"{user_card[:-20]} {get_mask_account(account)}"
+        return f"{user_card[:-20]}{get_mask_account(account)}"
     else:
         card_number = "".join(user_card[-16:].split())
-        return f"{user_card[:-16]} {get_mask_card_number(card_number)}"
+        return f"{user_card[:-16]}{get_mask_card_number(card_number)}"
 
 
-print(mask_account_card("visa 1234567890123456"))
-
-
-def get_date(date_string: str) -> str:
+def get_date(date_string: str) -> str | None:
     """Функция для возврата даты в привычном значении (день, месяц, год) из получаемой строки"""
     date = datetime.strptime(date_string, "%Y-%m-%dT%H:%M:%S.%f")
     return date.strftime("%d.%m.%Y")
