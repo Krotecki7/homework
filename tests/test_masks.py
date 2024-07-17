db06b9abe9892dc42c741cb5c1e2e37919ec84bc
Krotov <krot.playstation@gmail.com>
2024-07-17 18:04:31 +0300

    Небольшие поправки

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,27 +1,27 @@
 import pytest
 
-from src.masks import get_mask_card_number, get_mask_account
+from src.masks import get_mask_account, get_mask_card_number
 
 
 @pytest.fixture
-def mask_card():
-    return '7000792289606361'
+def mask_card() -> str:
+    return "7000792289606361"
 
 
-def test_get_mask_card_number(mask_card: str) -> str | None:
-    assert get_mask_card_number(mask_card) == '7000 79** **** 6361', "Неправильно набран номер"
+def test_get_mask_card_number(mask_card: str) -> None:
+    assert get_mask_card_number(mask_card) == "7000 79** **** 6361", "Неправильно набран номер"
     assert get_mask_card_number("") is None, "Введите номер карты"
-    assert get_mask_card_number('1234556778991234324123134') is None, "Слишком длинный номер"
-    assert get_mask_card_number('123455') is None, "Короткий номер"
+    assert get_mask_card_number("1234556778991234324123134") is None, "Слишком длинный номер"
+    assert get_mask_card_number("123455") is None, "Короткий номер"
 
 
 @pytest.fixture
-def mask_account():
-    return '73654108430135874305'
+def mask_account() -> str:
+    return "73654108430135874305"
 
 
-def test_get_mask_account(mask_account: str) -> str | None:
-    assert get_mask_account(mask_account) == '**4305', "Некорректный счет"
-    assert get_mask_account('') is None, "Введите номер карты"
-    assert get_mask_account('1234566787899121421553') is None, "Слишком длинный номер"
-    assert get_mask_account('123445') is None, "Короткий номер"
+def test_get_mask_account(mask_account: str) -> None:
+    assert get_mask_account(mask_account) == "**4305", "Некорректный счет"
+    assert get_mask_account("") is None, "Введите номер карты"
+    assert get_mask_account("1234566787899121421553") is None, "Слишком длинный номер"
+    assert get_mask_account("123445") is None, "Короткий номер"
--- tests/test_processing.py
+++ tests/test_processing.py
@@ -2,46 +2,55 @@ import pytest
 
 from src.processing import filter_by_state, sort_by_date
 
-list_of_dict = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-                {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-                {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+list_of_dict = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+]
 
 
+# noinspection PyTypeChecker
 @pytest.mark.parametrize(
     "list_of_dict, state, expected",
     [
-        (list_of_dict, "CANCELED",
-         [
-             {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-             {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-         ],
-         ),
-        (list_of_dict, "EXECUTED",
-         [
-             {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-             {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-         ],
-         ),
-        (list_of_dict, "ASSDSFS", [])
-    ]
+        (
+            list_of_dict,
+            "CANCELED",
+            [
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ],
+        ),
+        (
+            list_of_dict,
+            "EXECUTED",
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            ],
+        ),
+        (list_of_dict, "ASSDSFS", []),
+    ],
 )
-def test_filter_by_state(list_of_dict, state, expected):
+def test_filter_by_state(list_of_dict: list, state: str, expected: list) -> None:
     assert filter_by_state(list_of_dict, state) == expected
 
 
-def test_filter_by_state_():
+def test_filter_by_state_() -> None:
     assert filter_by_state(list_of_dict, "") == []
 
 
-def test_sort_by_date():
+def test_sort_by_date() -> None:
     assert sort_by_date(list_of_dict, ascending=True) == [
-        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
-        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ]
     assert sort_by_date(list_of_dict, ascending=False) == [
-        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
-        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}]
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    ]
