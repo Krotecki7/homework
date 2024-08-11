421c30e206509490a11f6e3c1ab2823a90b494f6
Krotov <krot.playstation@gmail.com>
2024-08-11 23:12:56 +0300

    Доработка функций и файла readme.md

--- src/masks.py
+++ src/masks.py
@@ -13,33 +13,29 @@ masks_logger.addHandler(file_handler)
 masks_logger.setLevel(logging.DEBUG)
 
 
-def get_mask_card_number(card_number: str) -> str | str:
+def get_mask_card_number(card_number: str) -> str | ValueError:
     """Функция маcкировки номера банковской карты"""
     masks_logger.info("Получаем данные карты")
     try:
         masks_logger.info("Маскируем карту")
         if card_number.isdigit() and len(card_number) == 16:
             return f"{card_number[:4]} {card_number[4:6]}{'*' * 2} {'*' * 4} {card_number[12:]}"
-    except Exception as ex:
-        masks_logger.error(f"Произошла ошибка: {ex}", exc_info=True)
-        return "Неверный ввод"
+        elif not (card_number.isdigit() and len(card_number) == 16):
+            raise ValueError("Неверный номер карты")
+    except ValueError as ex:
+        masks_logger.error(f"Произошла ошибка: {ex}")
+        return ex
 
 
-def get_mask_account(account_number: str) -> str | str:
+def get_mask_account(account_number: str) -> str | ValueError:
     """Функция маскировки лицевого счета"""
     masks_logger.info("Получаем данные лицевого счета")
     try:
-        masks_logger.info("Максируем номер лицевого счета")
+        masks_logger.info("Маскируем номер лицевого счета")
         if account_number.isdigit() and len(account_number) == 20:
             return f"{'*' * 2}{account_number[16:]}"
-    except Exception as ex:
-        masks_logger.error(f"произошла ошибка: {ex}", exc_info=True)
-        return "Неверный ввод"
-
-
-card_number = input("Введите номер карты: ")
-account_number = input("Введите номер лицевого счета: ")
-
-
-print(get_mask_card_number(card_number))
-print(get_mask_account(account_number))
+        elif not (account_number.isdigit() and len(account_number) == 20):
+            raise ValueError("Неверный номер лицевого счета")
+    except ValueError as ex:
+        masks_logger.error(f"Произошла ошибка: {ex}")
+        return ex
